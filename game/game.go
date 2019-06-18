a3921458bdb6564560917f9d9d6b2f3be6af5122
fralonra <fralonra@aliyun.com>
2019-06-18 23:37:03 +0800

    init

---
+++ game/game.go
@@ -0,0 +1,23 @@
+package game
+
+type board [4][4]int
+
+type Game struct {
+
+}
+
+func (g *Game) ToLeft() {
+
+}
+
+func (g *Game) ToRight() {
+
+}
+
+func (g *Game) ToTop() {
+
+}
+
+func (g *Game) ToBottom() {
+
+}
